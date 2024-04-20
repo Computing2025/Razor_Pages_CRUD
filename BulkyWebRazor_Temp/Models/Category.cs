079a59b2fd5d7058c7cbf6c4948c6ff9286d6367
Computing2025 <ozk8000@gmail.com>
2024-04-20 20:40:13 +0300

    Proje yeni repository ye taşındı

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "En fazla 30 karakter olmalıdır")]
+        [MinLength(3, ErrorMessage = "En az 3 karakter olmalıdır")]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "1 ile 100 arasında olmalıdır")]
+        public int DisplayOrder { get; set; }
+    }
+}
+
+
